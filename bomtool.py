d98ed2f379fccfe44d17ece2c138d12bc79ffd5f
Conor Peterson <conor.p.peterson@gmail.com>
2018-09-25 12:34:21 -0600

    Quantities and margin of error

--- bomtool.py
+++ bomtool.py
@@ -1,8 +1,8 @@
 import sys
+import math
 
 #Tool for generating Digikey fast-add compatible BOMs
 #from a copypasta in google sheets.
-
 
 def main():
     print("Paste in your part numbers on separate lines")
@@ -16,8 +16,14 @@ def main():
             break
         else:
             lines.append(part)
+    print("How many copies of this BOM?")
+    copies = int(input(">"))
+    print("Safety margin? (.05 = 5%)")
+    margin = float(input(">"))
+    if margin < 1.0:
+        margin += 1.0
     print("OK, here's your order.")
-    result = count(lines)
+    result = count(lines, copies, margin)
     output(result)
     summarize(result)
 
@@ -36,13 +42,19 @@ def summarize(d):
     print("BOM contains {} unique parts, {} total parts.".format(
         len(d.keys()), count))
 
-def count(items):
+def count(items, quantity=1, margin=1.0):
     result = dict()
     for i in items:
         if i in result.keys():
             result[i] += 1
         else:
             result[i] = 1
+    #Apply quantities
+    for i in result.keys():
+        q = result[i]
+        q *= quantity
+        q *= margin
+        result[i] = math.ceil(q)
     return result
     
 if __name__ == "__main__":
